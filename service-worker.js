fed1d7d0133a5c6f6e17defdb11446b39f29bf18
kztewpuu680795-cell <kztewpuu680795@gmail.com>
2025-10-18 16:06:15 +0800

    Update service worker to version 7

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿// service-worker.js
 
-const CACHE_NAME = 'direct-media-cache-v6';
+const CACHE_NAME = 'direct-media-cache-v7';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -64,6 +64,7 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
 
 
 
