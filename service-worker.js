0b0bb4bc456ec8d07ac90a6cd1af99aeaf548d04
kztewpuu680795-cell <kztewpuu680795@gmail.com>
2025-10-20 09:31:03 +0800

    Update service worker to version v12

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿// service-worker.js
 
-const CACHE_NAME = 'direct-media-cache-v11';
+const CACHE_NAME = 'direct-media-cache-v12';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -64,6 +64,7 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
 
 
 
