929ec7c697431e23270c2aed3b89c3f5672f3fbc
kztewpuu680795-cell <kztewpuu680795@gmail.com>
2025-10-26 16:53:13 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 ﻿// service-worker.js
 
-const CACHE_NAME = 'direct-media-cache-v001';
+const CACHE_NAME = 'direct-media-cache-v002';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -64,6 +64,7 @@ self.addEventListener('activate', (event) => {
     })
   );
 });
+
 
 
 
